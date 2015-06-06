285c6c0a93a3dc60226bf3e3852039cb69a41902
Khandelwal, Amitav <Amitav.Khandelwal@Staples.com>
2015-06-06 16:13:59 -0400

    Initial work on camera intents

--- app/src/main/java/com/busyscanner/busyscanner/HomeFragment.java
+++ app/src/main/java/com/busyscanner/busyscanner/HomeFragment.java
@@ -1,12 +1,24 @@
 package com.busyscanner.busyscanner;
 
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 /**
@@ -17,6 +29,9 @@ import android.widget.Button;
 public class HomeFragment extends Fragment implements View.OnClickListener {
 
     public static final String TAG = HomeFragment.class.getSimpleName();
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
 
     private Button takePicButton;
     private Button uploadButton;
@@ -49,6 +64,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         takePicButton = (Button) rootView.findViewById(R.id.pictureButton);
         uploadButton = (Button) rootView.findViewById(R.id.uploadButton);
 
+        takePicButton.setOnClickListener(this);
 
 
         return rootView;
@@ -61,8 +77,78 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 //TODO
                 break;
             case R.id.pictureButton:
-                //TODO
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                Uri fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+                // start the image capture Intent
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
                 break;
         }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                // Image captured and saved to fileUri specified in the Intent
+                Toast.makeText(getActivity(), "Image saved to:\n" +
+                        data.getData(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                // User cancelled the image capture
+                if (getView() != null) {
+                    Snackbar.make(getView(), getString(R.string.image_capture_cancelled),
+                            Snackbar.LENGTH_LONG).show();
+                }
+            } else {
+                // Image capture failed, advise user
+                Snackbar.make(getView(), getString(R.string.image_capture_failed),
+                        Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        Log.d(TAG, Environment.getExternalStorageState());
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM), "Busiscan");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d(TAG, "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
     }
 }
