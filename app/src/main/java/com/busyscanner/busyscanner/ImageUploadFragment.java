30dc66abc1e3318e3c4590a0aa85046a00c19a23
joe <Jschiarizzi@comcast.net>
2015-06-07 00:01:44 -0400

    add contact methods

--- app/src/main/java/com/busyscanner/busyscanner/ImageUploadFragment.java
+++ app/src/main/java/com/busyscanner/busyscanner/ImageUploadFragment.java
@@ -114,16 +114,27 @@ public class ImageUploadFragment extends Fragment implements Callback<List<BizCa
         Toast.makeText(getActivity(), "Image upload success", Toast.LENGTH_LONG).show();
         //TODO display what the JS says is on the card to ask the user for correctness
 
+        newContact(bizCardResponse);
     }
 
 
     //TODO add new contact
-    public void newContact(){
+    public void newContact(BizCardResponse card){
         // Creates a new Intent to insert a contact
         Intent intent = new Intent(android.provider.ContactsContract.Intents.Insert.ACTION);
         // Sets the MIME type to match the Contacts Provider
         intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
-        
+
+        private android.widget.EditText mEmailAddress = (android.widget.EditText) findViewById(R.id.email);
+        private android.widget.EditText mPhoneNumber = (android.widget.EditText) findViewById(R.id.phone);
+
+        intent.putExtra(ContactsContract.Intents.Insert.NAME, card.getName());//set required
+        intent.putExtra(ContactsContract.Intents.Insert.PHONE, card.getPhone());
+
+        if(card.getEmail() != null){intent.putExtra(ContactsContract.Intents.Insert.EMAIL, card.getEmail());}
+        if(card.getAddress() != null){intent.putExtra(ContactsContract.Intents.Insert.POSTAL, card.getAddress());}
+
+
     }
 
 
